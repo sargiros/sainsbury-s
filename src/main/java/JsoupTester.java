360b6313585903de46c01839de0f89c85cd2985a
Sokratis Argiros <socratis.argiros@gmail.com>
2018-05-04 20:27:39 +0300

    Add files via upload
    A trial java class that reads the links from the sainsburys web site and then loops over each one and selects specific data

--- src/main/java/JsoupTester.java
+++ src/main/java/JsoupTester.java
@@ -6,14 +6,16 @@ import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class JsoupTester {
 
+    private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static Object resultList;
+
 
     public static void main(String[] args) throws IOException {
 
@@ -27,34 +29,39 @@ public class JsoupTester {
             String alteredBerriesUrl = berriesUrl.replace("../", "");
             List<String> products = new ArrayList<>();
             products.add(alteredBerriesUrl);
-            /*get the title of the product*/
-            String title = element.text();
-            System.out.println(title);
-            for (String product : products) {
-                Document data = Jsoup.connect("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/" + alteredBerriesUrl).get();
-                final List<SearchResult> resultList = new ArrayList<>();
-                Elements relativeInfo = data.select("div#main");
-                for (Element newIfo : relativeInfo.select("div.productContent")) {
-                    String description = String.valueOf(newIfo.select("div#information .productText p:first-of-type").first());
 
-                    String newDesc = description.replace("<p>","" );
+
+
+            for (String product : products) {
+                Document data = Jsoup.connect("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/" + alteredBerriesUrl).userAgent(USER_AGENT).get();
+                Elements relativeInfo = data.select("div#main");
+                final List <SearchResult> resultList = new ArrayList<>();
+                for (Element newIfo : relativeInfo.select("div.productContent")) {
+                    /* Getting the product Description*/
+                    String desc = String.valueOf(newIfo.select("div#information .productText p:first-of-type").first());
+                    String newDesc = desc.replace("<p>","" );
                     String newDesc2 = newDesc.replace("</p>","" );
-                    String finalDesc = newDesc2;
-                    finalDesc.trim();
-                    System.out.println(finalDesc);
-                    String unit_price = String.valueOf(newIfo.select("p.pricePerUnit").text());
-                    System.out.println(unit_price);
+                    String description = newDesc2.trim();
+
+                    /*get the title of the product*/
+                    String title = String.valueOf(newIfo.select(".productTitleDescriptionContainer").text());
+
+
+                    String price_per_unit= String.valueOf(newIfo.select("p.pricePerUnit").text());
+                    String unit_price= price_per_unit.replace("/unit", "");
+
                     /*get the product kcals per 100gr*/
                     String kcal_per_100g1 = newIfo.select(".nutritionLevel1:first-child").text();
                     String kcal_per_100g2 = newIfo.select(".tableRow0:first-child").text();
                     String kcal_per_100g = kcal_per_100g1 + kcal_per_100g2;
-                    System.out.println(kcal_per_100g);
-                    //resultList.add(new SearchResult(title, kcal_per_100g));
+
+                    /*adding the information to the List*/
+                    resultList.add(new SearchResult(title, kcal_per_100g, unit_price, description));
                 }
 
+                OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(new File("results.json"), resultList);
             }
-
         }
-        OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(new File("results.json"), resultList);
+
     }
 }
